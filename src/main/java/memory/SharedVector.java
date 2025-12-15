049e1b059b5d2f05a217d1f11b3b2586c311028d
Hadar Yanai <yanaih@post.bgu.ac.il>
2025-12-15 21:06:09 +0200

    implemented first try of all sharedVEctor functions

--- src/main/java/memory/SharedVector.java
+++ src/main/java/memory/SharedVector.java
@@ -10,57 +10,96 @@ public class SharedVector {
 
     public SharedVector(double[] vector, VectorOrientation orientation) {
         // TODO: store vector data and its orientation
+        this.vector = vector;
+        this.orientation = orientation;
     }
 
     public double get(int index) {
         // TODO: return element at index (read-locked)
-        return 0;
+        // make sure number is in range??
+        return this.vector[index];
     }
 
     public int length() {
         // TODO: return vector length
-        return 0;
+        return this.vector.length;
     }
 
     public VectorOrientation getOrientation() {
         // TODO: return vector orientation
-        return null;
+        return this.orientation;
     }
 
     public void writeLock() {
         // TODO: acquire write lock
+        this.lock.writeLock().lock();
     }
 
     public void writeUnlock() {
         // TODO: release write lock
+        this.lock.writeLock().unlock();
     }
 
     public void readLock() {
         // TODO: acquire read lock
+        this.lock.readLock().lock();
     }
 
     public void readUnlock() {
         // TODO: release read lock
+        this.lock.readLock().unlock();
     }
 
     public void transpose() {
         // TODO: transpose vector
+        if (this.orientation == VectorOrientation.ROW_MAJOR) {
+            this.orientation = VectorOrientation.COLUMN_MAJOR;
+        } else {
+            this.orientation = VectorOrientation.ROW_MAJOR;
+        }
+    
     }
 
     public void add(SharedVector other) {
         // TODO: add two vectors
+        // make sure operation is legal
+        if (other == null) return;
+        if(other.vector != null && other.length() == this.length()) {
+            for(int i = 0; i <length(); i++ ) {
+                this.vector[i] = this.vector[i] + other.vector[i];
+                //does vector orientation matter??
+            }
+        }
+        return;
     }
 
     public void negate() {
         // TODO: negate vector
+         for(int i = 0; i <length(); i++ ) {
+                vector[i] = get(i) * -1;
+            }
     }
 
     public double dot(SharedVector other) {
         // TODO: compute dot product (row · column)
-        return 0;
+        //validation??
+        double sum = 0;
+        for (int i = 0; i < length(); i++) {
+            sum = sum + (get(i) * other.get(i));
+        }
+        return sum;
     }
 
     public void vecMatMul(SharedMatrix matrix) {
         // TODO: compute row-vector × matrix
+        //Validation
+        double[] product = new double[length()];
+        for (int i = 0; i < length(); i++) {
+            double sum = 0;
+            for (int j = 0; j < matrix.length(); j++) {
+            sum = sum + (get(i) * matrix.get(i).get(j));
+             }
+          product[i] = sum;
+        }
     }
 }
