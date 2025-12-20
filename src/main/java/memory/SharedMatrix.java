014f0052d2bbe83310b05e41a577f9f18315fa1c
bugv67 <noa67weiss@gmail.com>
2025-12-20 17:02:23 +0000

    sharedMatrix

--- src/main/java/memory/SharedMatrix.java
+++ src/main/java/memory/SharedMatrix.java
@@ -10,15 +10,47 @@ public class SharedMatrix {
     }
 
     public SharedMatrix(double[][] matrix) {
-        // TODO: construct matrix as row-major SharedVectors
+        // TODO: construct matrix as row-major SharedVectors shora
+        if (matrix == null || matrix.length == 0) {
+            this.vectors = new SharedVector[0];
+            return;
+        }
+        this.vectors = new SharedVector[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            this.vectors[i] = new SharedVector(matrix[i], VectorOrientation.ROW_MAJOR);
+        }
     }
 
     public void loadRowMajor(double[][] matrix) {
         // TODO: replace internal data with new row-major matrix
+        acquireAllVectorWriteLocks(vectors); // so that other wont work on un updated data
+        SharedVector[] newMarix = new SharedVector[matrix.length];
+        SharedVector[] oldMarix = this.vectors;
+        try {
+
+            for (int i = 0; i < matrix.length; i++) {
+                newMarix[i] = new SharedVector(matrix[i], VectorOrientation.ROW_MAJOR);
+            }
+            this.vectors = newMarix;
+        } finally {
+            releaseAllVectorWriteLocks(oldMarix);
+        }
     }
 
     public void loadColumnMajor(double[][] matrix) {
         // TODO: replace internal data with new column-major matrix
+        acquireAllVectorWriteLocks(vectors); // so that other wont work on un updated data
+        // do we need to flip the data? becuse each row is a vector
+        SharedVector[] newMarix = new SharedVector[matrix.length];
+        SharedVector[] oldMarix = this.vectors;
+        try {
+            for (int i = 0; i < matrix.length; i++) {
+                newMarix[i] = new SharedVector(matrix[i], VectorOrientation.COLUMN_MAJOR);
+            }
+            this.vectors = newMarix;
+        } finally {
+            releaseAllVectorWriteLocks(oldMarix);
+        }
     }
 
     public double[][] readRowMajor() {
@@ -28,32 +60,60 @@ public class SharedMatrix {
 
     public SharedVector get(int index) {
         // TODO: return vector at index
-        return null;
+
+        if (index < 0 || index > vectors.length) {
+            throw new IndexOutOfBoundsException("Invalid index: " + index);
+        }
+        vectors[index].readLock();
+        try {
+            return vectors[index];
+        } finally {
+            vectors[index].readUnlock();
+        }
     }
 
     public int length() {
         // TODO: return number of stored vectors
-        return 0;
+        if (this.vectors == null) {
+            return 0;
+        }
+        return this.vectors.length;
     }
 
     public VectorOrientation getOrientation() {
         // TODO: return orientation
-        return null;
+        // what to do if null or empty??
+        if (this.vectors == null || this.vectors.length == 0) {
+            return VectorOrientation.ROW_MAJOR; // default to row major if empty
+        }
+        return vectors[0].getOrientation();
     }
 
     private void acquireAllVectorReadLocks(SharedVector[] vecs) {
         // TODO: acquire read lock for each vector
+        for (int i = 0; i < vecs.length; i++) {
+            vecs[i].readLock();
+        }
     }
 
     private void releaseAllVectorReadLocks(SharedVector[] vecs) {
         // TODO: release read locks GHHHHH
+        for (int i = 0; i < vecs.length; i++) {
+            vecs[i].readUnlock();
+        }
     }
 
     private void acquireAllVectorWriteLocks(SharedVector[] vecs) {
         // TODO: acquire write lock for each vector
+        for (int i = 0; i < vecs.length; i++) {
+            vecs[i].writeLock();
+        }
     }
 
     private void releaseAllVectorWriteLocks(SharedVector[] vecs) {
         // TODO: release write locks
+        for (int i = 0; i < vecs.length; i++) {
+            vecs[i].writeUnlock();
+        }
     }
 }
