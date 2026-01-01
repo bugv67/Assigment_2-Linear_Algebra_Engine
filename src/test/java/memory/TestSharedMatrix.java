ca60fa03f9343c8a27575c06c86ab67dfefb004a
Hadar Yanai <yanaih@post.bgu.ac.il>
2026-01-01 19:18:06 +0200

    finished

--- src/test/java/memory/TestSharedMatrix.java
+++ src/test/java/memory/TestSharedMatrix.java
@@ -3,8 +3,6 @@ package memory;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Nested;
-
 class SharedMatrixTest {
  
 // TEST CONSTRUCTORS
--- src/test/java/spl/lae/TestLinearAlgebraEngine.java
+++
@@ -1,5 +0,0 @@
-package spl.lae;
-
-public class TestLinearAlgebraEngine {
-
-}
